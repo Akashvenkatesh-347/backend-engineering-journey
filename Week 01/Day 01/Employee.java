0006288edb8fa4894ac7142b27f1ef0b263e42b6
Akashvenkatesh-347 <akashvenkatesh347@gmail.com>
2026-06-24 19:05:52 +0530

    Bonus Calculation Completed

---
+++ Week 01/Day 01/Employee.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Employee {
+
+    public void calculateBonus(int Salary, char Ratings) {
+        int Bonus;
+        if (Ratings == 'A') {
+            Bonus = (int)(Salary * 0.20);
+        } else if (Ratings == 'B') {
+            Bonus = (int)(Salary * 0.10);
+        } else if (Ratings == 'C') {
+            Bonus = (int)(Salary * 0.05);
+        } else {
+            Bonus = 0;
+        }
+        System.out.println("The bonus of the Employee is " + Bonus);
+    }
+
+    public void printEmpReport(String EmpName, int Salary, char Ratings) {
+        System.out.println("The Name of the Employee is " + EmpName);
+        System.out.println("The Salary of the Employee is " + Salary);
+        System.out.println("The Performance Ratings of the Employee is " + Ratings);
+    }
+
+    public static void main(String[] args) {
+        Employee emp = new Employee();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the Employee Name:");
+        String EmpName = scanner.nextLine();
+        System.out.println("Enter the Salary:");
+        int Salary = scanner.nextInt();
+        System.out.println("Enter the Ratings(A/B/C/D):");
+        char Ratings = scanner.next().charAt(0);
+        emp.calculateBonus(Salary,Ratings);
+        emp.printEmpReport(EmpName,Salary,Ratings);
+    }
+
+}
