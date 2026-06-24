458198105f31bd2930b8b2d16b2974f55b44a71e
Akashvenkatesh-347 <akashvenkatesh347@gmail.com>
2026-06-24 19:49:29 +0530

    Employee.java file updated

--- Week 01/Day 01/Employee.java
+++ Week 01/Day 01/Employee.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Employee {
 
-    public void calculateBonus(int Salary, char Ratings) {
+    public int calculateBonus(int Salary, char Ratings) {
         int Bonus;
         if (Ratings == 'A') {
             Bonus = (int)(Salary * 0.20);
@@ -13,13 +13,15 @@ public class Employee {
         } else {
             Bonus = 0;
         }
-        System.out.println("The bonus of the Employee is " + Bonus);
+        return Bonus;
     }
 
-    public void printEmpReport(String EmpName, int Salary, char Ratings) {
+    public void printEmpReport(String EmpName, int Salary, char Ratings,int Bonus) {
         System.out.println("The Name of the Employee is " + EmpName);
         System.out.println("The Salary of the Employee is " + Salary);
         System.out.println("The Performance Ratings of the Employee is " + Ratings);
+        System.out.println("The Bonus of the Employee is "+ Bonus);
+        System.out.println("The Final Salary of the Employee is "+(Salary+Bonus));
     }
 
     public static void main(String[] args) {
@@ -31,8 +33,8 @@ public class Employee {
         int Salary = scanner.nextInt();
         System.out.println("Enter the Ratings(A/B/C/D):");
         char Ratings = scanner.next().charAt(0);
-        emp.calculateBonus(Salary,Ratings);
-        emp.printEmpReport(EmpName,Salary,Ratings);
+        int Bonus = emp.calculateBonus(Salary,Ratings);
+        emp.printEmpReport(EmpName,Salary,Ratings,Bonus);
     }
 
 }
